2493629557568e30efd4ebcd9aaec06b2d1887f2
Radin madad nezhad aligorkeh <rad1383in@gmail.com>
2025-01-24 16:06:07 -0500

    full game logic

--- lab03.py
+++ lab03.py
@@ -1,3 +1,4 @@
+import random
 #Dice Options created using list() and range()
 diceOptions = list(range(1,7))
 
@@ -13,3 +14,26 @@ def getCombatStrength(prompt):
             return value    
         else:
             print("Invalid value. Please enter a number between 1 and 6.")
+combatStrength = getCombatStrength("Enter a number between 1-6 for Player")
+mCombatStrength = getCombatStrength("Enter a number between 1-6 for Monster")
+
+for j in range(1, 21, 2):
+    heroRoll = random.choice(diceOptions)
+    monsterRoll = random.choice(diceOptions)
+
+    heroWeapon = weapons[heroRoll - 1]
+    monsterWeapon = weapons[monsterRoll - 1]
+
+    heroTotal = combatStrength + heroRoll
+    monsterTotal = mCombatStrength + monsterRoll
+
+    print(f"\n Hero rolled {heroRoll}, Monster rolled {monsterRoll}")
+    print(f"\n Hero Selected {heroWeapon}, Monster selected {monsterWeapon}")
+    print(f"\n Hero total strength is {heroTotal}, Monster total strength is {monsterTotal}")
+    if heroTotal > monsterTotal:
+        print("Player wins!")
+    elif heroTotal < monsterTotal:
+        print("Monster wins!")
+    else:
+        print("It's a tie!")
+    
