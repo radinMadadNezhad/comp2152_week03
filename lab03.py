2a4a02ce257f410d35ee857ec496245dc89df9ff
Radin madad nezhad aligorkeh <rad1383in@gmail.com>
2025-01-24 15:55:44 -0500

    update the rep with lab03.py

--- lab03.py
+++ lab03.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+#Dice Options created using list() and range()
+diceOptions = list(range(1,7))
+
+# Weapon list
+weapons = ["Fist", "Knife", "Club", "Gun", "Bomb", "Nuclear Bomb"]
+
+print("Availabe weapons: ", ', '.join(weapons))
+
+def getCombatStrength(prompt):
+    while True:
+        value = int(input(prompt))
+        if 1<= value <= 6:
+            return value    
+        else:
+            print("Invalid value. Please enter a number between 1 and 6.")
